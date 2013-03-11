f3d2028326010ed6780e8d0abf92e437791f473d
Hsiaoming Yang <lepture@me.com>
2013-03-11 23:00:33 +0800

    refactor

--- src/github.js
+++ src/github.js
@@ -1,104 +1,98 @@
 // GitHub API v3
 // ==============
+
 define(function(require, exports, module) {
-    var API_BASE = 'https://api.github.com';
-    var CALLBACK = 'callback=define';
+  var API_BASE = 'https://api.github.com';
+  var CALLBACK = 'callback=define';
 
-    function github(name) {
-        if (!(this instanceof github)) {
-            return new github(name);
-        }
-
-        var bits = name.split('/');
-        this.user = bits[0];
-        if (bits.length == 2) {
-            this.repo = bits[1];
-        } else {
-            this.repo = null;
-        }
+  function github(name) {
+    if (!(this instanceof github)) {
+      return new github(name);
     }
 
-    github.prototype.repos = function(options) {
-        options = options || {};
+    var bits = name.split('/');
+    this.user = bits[0];
+    if (bits.length == 2) {
+      this.repo = bits[1];
+    } else {
+      this.repo = null;
+    }
+  }
 
-        var url = API_BASE + '/users/' + this.user;
-        url += '/repos?' + CALLBACK;
-        var qs = toQuery(
-            options,
-            ['type:all', 'sort:updated', 'page', 'per_page:10']
-        )
-        url += '&' + qs;
+  github.prototype.repos = function(options) {
+    var path = '/users/' + this.user + '/repos';
+    request(
+      path, options, 
+      ['type:all', 'sort:updated', 'page', 'per_page:10']
+    );
+    return this;
+  }
 
-        require.async(url, function(response) {
-            options.callback(response);
-        });
-        return this;
+  github.prototype.tags = function(options) {
+    options = options || {};
+    var repo = options.repo || this.repo;
+    var path = '/repos/' + this.user + '/' + repo + '/tags';
+    request(path, options);
+    return this;
+  }
+
+  github.prototype.commits = function(options) {
+    options = options || {};
+    var repo = options.repo || this.repo;
+
+    var path = '/repos/' + this.user + '/' + repo + '/commits';
+    request(path, options, ['page', 'per_page:10']);
+    return this;
+  }
+
+  github.prototype.issues = function(options) {
+    options = options || {};
+    var repo = options.repo || this.repo;
+
+    var path = '/repos/' + this.user + '/' + repo + '/issues';
+    if (options.issueId) {
+      path = path + '/' + options.issueId;
     }
 
-    github.prototype.tags = function(options) {
-        options = options || {};
-        var repo = options.repo || this.repo;
+    request(
+      path,
+      options,
+      [
+        'sort:updated', 'state', 'milestone', 'labels',
+        'page', 'per_page:10'
+      ]
+    );
+    return this;
+  }
 
-        var url = API_BASE + '/repos/' + this.user + '/' + repo;
-        url += '/tags?' + CALLBACK;
-        require.async(url, function(response) {
-            options.callback(response);
-        });
-        return this;
+  // Helpers
+  // ----------------
+  function toQuery(options, keys) {
+    // toQuery(options, ['state', 'milestone']);
+    var query = [];
+    for (var i = 0; i < keys.length; i++) {
+      var bits = keys[i].split(':');
+      var key = bits[0];
+      (function() {
+        var value;
+        if (bits.length > 1) value = bits[1];
+        value = options[key] || value;
+        if (value) query.push(key + '=' + value);
+      })();
     }
+    return query.join('&');
+  }
 
-    github.prototype.commits = function(options) {
-        options = options || {};
-        var repo = options.repo || this.repo;
+  function request(path, options, queryFields) {
+    options = options || {};
 
-        var url = API_BASE + '/repos/' + this.user + '/' + repo;
-        url += '/commits?' + CALLBACK;
-        url += '&' + toQuery(options, ['page', 'per_page:10']);
-
-        require.async(url, function(response) {
-            options.callback(response);
-        });
-        return this;
+    var url = 'https://api.github.com' + path + '?callback=define';
+    if (queryFields) {
+      var qs = toQuery(options, queryFields);
+      url += '&' + qs;
     }
+    require.async(url, options.callback);
+  }
 
-    github.prototype.issues = function(options) {
-        options = options || {};
-        var repo = options.repo || this.repo;
-
-        var url = API_BASE + '/repos/' + this.user + '/' + repo;
-        url += '/issues?' + CALLBACK;
-        var qs = toQuery(
-            options,
-            [
-                'sort:updated', 'state', 'milestone', 'labels',
-                'page', 'per_page:10'
-            ]
-        );
-        url += '&' + qs;
-
-        require.async(url, function(response) {
-            options.callback(response);
-        });
-        return this;
-    }
-
-    // Helpers
-    // ----------------
-    function toQuery(options, keys) {
-        // toQuery(options, ['state', 'milestone']);
-        var query = [];
-        for (var i = 0; i < keys.length; i++) {
-            var bits = keys[i].split(':');
-            var key = bits[0];
-            (function() {
-                var value;
-                if (bits.length > 1) value = bits[1];
-                value = options[key] || value;
-                if (value) query.push(key + '=' + value);
-            })();
-        }
-        return query.join('&');
-    }
-
-    module.exports = github;
+  module.exports = github;
 });
